b6a16da88f5815c982b50c1e95105c791e020826
Rasmus Ståhl <rasmus.stahl.47@gmail.com>
2018-02-25 15:13:57 +0100

    added wordCloud (beta) and a new style-sheet

---
+++ source/static/js/config.js
@@ -0,0 +1,12 @@
+
+
+requirejs.config({
+    baseUrl: 'lib',
+    paths: {
+        index: 'd3-cloud-master/index',
+        buildfile: 'd3-cloud-master(build/d3.layout.cloud',
+        d3: 'd3',
+        d3min: 'd3.min'
+
+    }
+});
