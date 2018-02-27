ac8199c299eeece0092fe40fc90084e35e449273
tobma571 <tobma571@student.liu.se>
2018-02-27 17:11:13 +0100

    some progress with the treemap

--- source/static/js/tm.js
+++ source/static/js/tm.js
@@ -4,9 +4,12 @@ function tm(data,data_category)
     var div = '#treemap';
 
     var smaller_data = data.slice(0,10);
+    var categories = data_category;
 
     var parentWidth = $(div).width();
     var parentHeight = $(div).height();
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
 
     var margin = {top: 0, right: 3, bottom: 0, left: 0},
         width = parentWidth - margin.left - margin.right,
@@ -22,30 +25,67 @@ function tm(data,data_category)
 
     var cat_rate = [];
 
-    for(var i = 0; i < data_category.items.length; i++)
+
+    for(var i = 0; i < categories.items.length; i++)
     {
         var sum = 0;
         for(var j = 0; j < smaller_data.length; j++)
         {
-            if(data_category.items[i].id == smaller_data[j].category_id)
+            if(categories.items[i].id == smaller_data[j].category_id) {
                 sum++;
+                //smaller_data[j].category = data_category.items[i].snippet.title;
+            }
         }
-        cat_rate.push(sum);
+        categories.items[i].occurence = sum;
+
     }
 
 
-    var categories = data_category.items.map(function (value, i) {
+
+    /*var categories = data_category.items.map(function (value, i) {
         return {"id": value.id, "title": value.snippet.title, "rate": cat_rate[i]};
-    });
+    });*/
+
 
 
     /*var stratify = d3.stratify()
         .parentId(function (d) {
-            return d.title;
-        })
+            return d.category_id;
+        });
+*/
+console.log(categories);
+    var root = d3.hierarchy(categories)
+        .sum(function(d) { return d.items.occurence; })
+        .sort(function(a, b) { return b.height - a.height || b.value - a.value; });
 
-    var root = stratify(categories)
-        .sum(function(d) { return d.rate; })
-        .sort(function(a, b) { return b.height - a.height || b.value - a.value; });*/
+    treeMap(root);
+    console.log(root.leaves());
+
+        var cell = svg.selectAll("g")
+            .data(root.leaves())
+            .enter().append("g")
+            .attr("transform", function(d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
+
+        cell.append("rect")
+            .attr("id", function(d,i) { return d.data.items.id; })
+            .attr("width", function(d) { return d.x1 - d.x0; })
+            .attr("height", function(d) { return d.y1 - d.y0; })
+            .attr("fill", function(d,i) { return color(d.data.items.id); });
+/*
+
+        .attr("class", "node")
+        .attr("title", function(d,i) { return d.data.items[i].snippet.title; })
+        .style("left", function(d) { return d.x0 + "px"; })
+        .style("top", function(d) { return d.y0 + "px"; })
+        .style("width", function(d) { return d.x1 - d.x0 + "px"; })
+        .style("height", function(d) { return d.y1 - d.y0 + "px"; })
+        .style("background", function(d) { return color(d.data.items.id); })
+        .append("div")
+        .attr("class", "node-label")
+        .text(function(d,i) { return d.data.items[i].snippet.title; })
+        .append("div")
+        .attr("class", "node-value")
+        .text(function(d,i) { return d.data.items[i].snippet.occurence; });*/
 
 };
+
