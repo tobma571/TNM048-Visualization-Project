164a095a9f3c82a7948bf4efa4aabbaae27167ef
Anton Sterner <anton.sterner@gmail.com>
2018-02-28 13:52:02 +0100

    testing

--- source/static/js/main.js
+++ source/static/js/main.js
@@ -9,32 +9,97 @@ queue()
 
 var wordCloud;
 var tm;
+var time, country;
+var USvideos;//, CAvideos, GBvideos;
+var US_category, CA_category, GB_category;
 
-function draw(error, USvideos, CAvideos, GBvideos, 
+
+
+
+function draw(USvideos, CAvideos, GBvideos, 
 	US_category_id, CA_category_id, GB_category_id){
-if(error){ console.log(error); }
 
 
-	USvideos = transformData(USvideos);
-	CAvideos = transformData(CAvideos);
-	GBvideos = transformData(GBvideos);
+	// USvideos = transformData(USvideos);
+	// CAvideos = transformData(CAvideos);
+	// GBvideos = transformData(GBvideos);
+	// US_category = US_category_id;
+	// CA_category = CA_category_id;
+	// GB_category = GB_category_id;
+	// console.log(US_category);
+	console.log("hej 2");
+	//var format = d3.timeParse("%Y-%m-%dT%H:%M:%S.%LZ");
+	//var timeStart = 
+
+	//tm = new tm(USvideos,US_category_id);
 
 
-tm = new tm(USvideos,US_category_id);
-
-
-	wordCloud = new wordCloud(USvideos, CAvideos, GBvideos, 
-	US_category_id, CA_category_id, GB_category_id);
+	//wordCloud = new wordCloud(USvideos, US_category_id);
 	
 
 }
 
 // add a measure of popularity in data
 function transformData(data){
-	var dim = Object.keys(data[0]); // features/dimensions of dataset
+	// var dim = Object.keys(data[0]); // features/dimensions of dataset
 	data.forEach(function(d){
 		d.rating = Math.log2(d.views) + Math.log(d.likes/d.dislikes)
 				+ Math.log(d.comment_count); 
 	})
 	return data;
 }
+
+function filterData() {
+
+}
+
+function clickUS() {
+  	country = document.getElementById('US').getAttribute('value');
+  	$( "#treemap" ).empty();
+
+  	// if svg element is not empty, remove()
+ 	d3.csv("static/data/youtube-new/USvideos.csv", function(data) {
+  	USvideos = new transformData(data);
+  	delete wordCloud;
+  	wordCloud = new wordCloud(USvideos, US_category);
+	});
+ 
+
+}
+function clickCA() {
+  country = document.getElementById('CA').getAttribute('value');
+  $( "#treemap" ).empty();
+  // tm = new tm(CAvideos,CA_category_id);
+   	d3.csv("static/data/youtube-new/CAvideos.csv", function(data) {
+  	CAvideos = new transformData(data);
+  	delete wordCloud;
+  	wordCloud = new wordCloud(CAvideos, CA_category);
+  });
+}
+function clickGB() {
+  country = document.getElementById('GB').getAttribute('value');
+   // tm = new tm(GBvideos,GB_category_id);
+  d3.csv("static/data/youtube-new/GBvideos.csv", function(data) {
+  	GBvideos = new transformData(data);
+    wordCloud = new wordCloud(GBvideos, GB_category);
+    });
+}
+
+function clickTime1() {
+  time = document.getElementById('1week').getAttribute('value');
+  console.log(time);
+}
+function clickTime2() {
+  time = document.getElementById('2week').getAttribute('value');
+  console.log(time);
+}
+function clickTime3() {
+  time = document.getElementById('1month').getAttribute('value');
+  console.log(time);
+}
+function clickTime4() {
+  time = document.getElementById('3month').getAttribute('value');
+  console.log(time);
+}
+
+
